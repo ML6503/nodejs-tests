8e6f7cd88d9ddc816f2cc360cc22e1a423a99028
ML6503 <ml6503@gmail.com>
2022-11-19 23:12:24 +0200

    refactor: double and sum func tests

--- tests/doubleAndSum.test.js
+++ tests/doubleAndSum.test.js
@@ -2,23 +2,25 @@ import { test } from 'node:test';
 import assert from 'node:assert';
 import { doubleAndSum } from '../tasks/doubleAndSum.mjs';
 
-test("Check if it doubles each element or the array and returns the sum of the doubled numbers", async (t) => {
-  await t.test("Array of Length 3", (t) => {
-    assert.strictEqual(doubleAndSum([1, 2, 3]), 12);
+test("Check if doubleAndSum function doubles each element or the array and returns the sum of the doubled numbers", async (t) => {
+  let result;
+  await t.test("Array of 1, 2, 3 ", (t) => {
+    result = 12;
+    assert.strictEqual(doubleAndSum([1, 2, 3]), result);
   });
 
   await t.test("Array of Length 10", (t) => {
-    assert.strictEqual(doubleAndSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), 90);
+    result = 90;
+    assert.strictEqual(doubleAndSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), result);
   });
 
   await t.test("Array of random large numbers", (t) => {
+    result = 2028;
     assert.strictEqual(
       doubleAndSum([
         11, 24, 43, 4, 85, 6, 117, 8, 9, 510, 22, 4, 7, 9, 33, 45, 77,
       ]),
-      2028
+      result
     );
   });
 });
-
-// test courtesy of https://sweetcode.io/the-nodejs-test-runner-module/
