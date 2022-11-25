66298641b02af8e37e2d853d4c9db418b218803c
ML6503 <ml6503@gmail.com>
2022-11-26 00:27:48 +0200

    fix: test tescr for fetch

--- tests/fetch.test.js
+++ tests/fetch.test.js
@@ -2,21 +2,18 @@ import { test, describe } from 'node:test';
 import assert from 'node:assert';
 import { getNodejsDocsSource } from '../tasks/fetch.mjs';
 
-
 describe('Check fetch api node.js docs source function is working', () => {
-
-  test('getNodejsDocsSource function returns  is called once', async () => {
+  test('getNodejsDocsSource function returns "doc/api/documentation.md" and is called once', async () => {
     const tracker = new assert.CallTracker();
     const fetchFunc = tracker.calls(getNodejsDocsSource, 1);
     const nodeApiDocReceivedSource = await fetchFunc();
-   
+
     const nodeApiDocExpectedSource = 'doc/api/documentation.md';
 
-    assert.equal(nodeApiDocReceivedSource,  nodeApiDocExpectedSource);
+    assert.equal(nodeApiDocReceivedSource, nodeApiDocExpectedSource);
 
     process.on('exit', () => {
       tracker.verify();
     });
   });
 });
-
